1ac06e2a673eece381f77aa861f906d3e91ee760
jaimeacevedo539 <jaimeacevedo539@gmail.com>
2022-10-31 16:05:59 -0500

    se agrega el proyecto

---
+++ js/calculadora.js
@@ -0,0 +1,41 @@
+window.addEventListener('load', ()=> {
+    const display = document.querySelector('.calculadora-display');
+    const keypadbuttons = document.getElementsByClassName('keypad-buttons');
+
+    const keypadbuttonsArray = Array.from(keypadbuttons);
+
+    keypadbuttonsArray.forEach((button) => {
+        button.addEventListener('click', ()=>{
+            calculadora(button, display)
+        })
+    })
+ })
+
+ function calculadora(button, display){
+    switch(button.innerHTML){
+        case 'C':
+            borrar(display);
+            break;
+        case '=':
+            calcular(display);
+            break;
+        default:
+            actualizar(display,button);
+            break;
+    }
+}
+
+function calcular(display){
+    display.innerHTML = eval(display.innerHTML)
+}
+
+function actualizar(display, button){
+    if(display.innerHTML == 0){
+        display.innerHTML = ' ';
+    }
+    display.innerHTML += button.innerHTML;
+}
+
+function borrar(display){
+    display.innerHTML = 0;
+}
